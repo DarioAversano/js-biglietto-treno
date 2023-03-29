2cb03fbc22ca87775156c01fd7c5cac7eb90f48d
DarioAversano <risusveloce@gmail.com>
2023-03-29 17:35:29 +0200

    commit fine esercizio

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 let km = prompt("enter the number of kilometers to travel:");
 let age = prompt("enter your age:");
 
-
 km = parseInt(km);
 age = parseInt(age);
 let IsKmANumber = !isNaN(km);
@@ -10,33 +9,18 @@ let IsAgeANumber = !isNaN(age);
 let kmprice = 0.21;
 let balance = km * kmprice;
 
-consol.log("Price to pay"+ balance);
+console.log("Price to pay: " + balance.toFixed(2) + " euro");
 
-let discount65 = balance / 100 * 60
-let discountminor = balance / 100 * 80
+let discount65 = balance / 100 * 60;
+let discountminor = balance / 100 * 80;
 
-if (age > 65) {
-    console.log("your special price is:" + discount65); 
-    document.getElementById("discount65").innerHTML = discount65.toFixed(2)+"euro";
-  } else if (eta < 18) {
-    console.log("your special price is:" + discountminor); 
-    document.getElementById("discountminor").innerHTML = discountminor.toFixed(2)+"euro";
-  } else {
+if(age > 65){
+    console.log("your special price is: " + discount65); 
+    document.getElementById("balance").innerHTML = discount65.toFixed(2) + " euro";
+  }else if (age < 18){
+    console.log("your special price is: " + discountminor); 
+    document.getElementById("balance").innerHTML = discountminor.toFixed(2) + " euro";
+  }else{
     console.log(balance);
-    document.getElementById("balance").innerHTML = "your ticket price is" + balance.toFixed(2)+"euro";
+    document.getElementById("balance").innerHTML = "your ticket price is " + balance.toFixed(2) + " euro";
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
