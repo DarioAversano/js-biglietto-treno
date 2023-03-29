64d9686963ba5d1249bade8ef06f493e5f56d6e8
DarioAversano <risusveloce@gmail.com>
2023-03-29 16:27:21 +0200

    commit1

---
+++ js/main.js
@@ -0,0 +1,45 @@
+
+const kmprice = 0.21;
+let km = prompt("enter the number of kilometers to travel:");
+let IsKmANumber = !isNaN(km);
+let balance = km * kmprice 
+km = parseInt(km);
+
+
+let age = prompt("enter your age:");
+let IsAgeANumber = !isNaN(age);
+age = parseInt(age);
+
+
+if (age > 65) {
+    balance = balance * 0.8; 
+  } else if (eta < 18) {
+    balance = balance * 0.6; 
+  } else {
+    balance == balance 
+  }
+
+  let ticketprice = `
+Price to pay:
+> ${ticketprice}$
+
+---------------------------------------------
+`;
+
+console.log(ticketprice);
+document.getElementById("finalprice").innerHTML = ticketprice;
+  
+
+  alert("Ticketprice: " + ticketprice.toFixed(2) + " euro");
+
+
+
+
+
+
+
+
+
+
+
+
